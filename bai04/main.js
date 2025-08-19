9061f943a74d3b71c853e9977094e4e8b08475e0
Nghia-opp <nt14499@gmail.com>
2025-08-19 23:45:00 +0900

    Upload bài tập SS10 bài 04

---
+++ bai04/main.js
@@ -0,0 +1,6 @@
+let firstNumber = Number(prompt("First number :"));
+let secondNumber = Number(prompt("Second number :"));
+
+let totalSum = firstNumber + secondNumber;
+
+alert("Tổng 2 số bằng" + totalSum);
